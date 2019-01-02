11f9485416951bacd5f13e1399541d5b8edcc5e0
Alex Olieman <alex@olieman.net>
2019-01-02 21:42:44 +0100

    fix parser

--- dbpedia/parser.py
+++ dbpedia/parser.py
@@ -11,6 +11,12 @@ class NTriplesParser(rdflib_nt.NTriplesParser):
     Last updated for rdflib 5.0.0.dev0
     """
 
+    def __init__(self, sink=None):
+        super(self.__init__(sink=sink))
+        self.file = None
+        self.buffer = None
+        self.line = None
+
     def parse(self, f):
         """Parse f as an N-Triples file."""
         if not hasattr(f, 'read'):
@@ -18,15 +24,15 @@ class NTriplesParser(rdflib_nt.NTriplesParser):
 
         # since N-Triples 1.1 files can and should be utf-8 encoded
         f = codecs.getreader('utf-8')(f)
-
         self.file = f
         self.buffer = ''
+
         while True:
-            line = self.readline()
-            if line is None:
+            self.line = self.readline()
+            if self.line is None:
                 break
             try:
                 self.parseline()
             except rdflib_nt.ParseError:
-                raise print(f'Invalid line: {line}', file=sys.stderr)
+                raise print(f'Invalid line: {self.line}', file=sys.stderr)
         return self.sink
