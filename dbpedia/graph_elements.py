0d75b2b92376c59811e15f907699ddbfc2de6556
Alex Olieman <alex@olieman.net>
2019-03-20 14:41:53 +0100

    move prefixer and uri lookup to separate file; connection reuse for STC

--- dbpedia/graph_elements.py
+++ dbpedia/graph_elements.py
@@ -1,21 +1,16 @@
-import functools
 import glob
 import json
 import multiprocessing
 import os
-import re
 import sys
-from collections import Counter, defaultdict, UserDict
+from collections import Counter, defaultdict
 
-import requests
-from bs4 import BeautifulSoup
 from rdflib import Literal
-from requests import RequestException
 from tqdm import tqdm
 
 from dbpedia.compute_parts import compute_parts
 from dbpedia.parser import NTriplesParser
-from dbpedia.utils import base_path
+from dbpedia.uri_transformation import NamespacePrefixer, SameThingClient
 
 OWL_SAME_AS = 'http://www.w3.org/2002/07/owl#sameAs'
 MULTIVALUED_URI_PROPS = {
@@ -126,11 +121,14 @@ class PropertyGraphSink:
         self.global_id_marker = global_id_marker
         self.part_name = part_name
         self.prefixer = prefixer
-        self.samething_service = samething_service
         self.predicate_count = Counter()
         self.vertex_buffer = defaultdict(list)
         self.edge_buffer = []
         self.last_subject = None
+
+        self.samething_client = None
+        if samething_service:
+            self.samething_client = SameThingClient(samething_service)
 
     def __enter__(self):
         if glob.glob(f'{self.part_name}*'):
@@ -172,9 +170,9 @@ class PropertyGraphSink:
         elif self.global_id_marker in obj:
             # create an edge
             wd_subj, wd_obj = None, None
-            if self.samething_service:
-                wd_subj = fetch_wikidata_uri(self.samething_service, subj)
-                wd_obj = fetch_wikidata_uri(self.samething_service, obj)
+            if self.samething_client:
+                wd_subj = self.samething_client.fetch_wikidata_uri(subj)
+                wd_obj = self.samething_client.fetch_wikidata_uri(obj)
                 if self.prefixer:
                     wd_subj = self.prefixer.qname(wd_subj)
                     wd_obj = self.prefixer.qname(wd_obj)
@@ -234,8 +232,8 @@ class PropertyGraphSink:
 
     def flush_vertex(self):
         if self.vertex_buffer:
-            if self.samething_service:
-                wd_subj = fetch_wikidata_uri(self.samething_service, self.last_subject)
+            if self.samething_client:
+                wd_subj = self.samething_client.fetch_wikidata_uri(self.last_subject)
                 if self.prefixer:
                     wd_subj = self.prefixer.qname(wd_subj)
                 self.vertex_buffer['dbg:cluster-id'] = self.vertex_buffer['id']
@@ -262,110 +260,3 @@ class PropertyGraphSink:
             'value': value,
             'language': language
         }
-
-
-class NamespacePrefixer(UserDict):
-
-    def __init__(self, mapping=None, **kwargs):
-        super().__init__(mapping, **kwargs)
-        self.default_namespaces_url = 'http://dbpedia.org/sparql?nsdecl'
-        self.default_namespaces_file = base_path('default-namespaces.json')
-        if not self.data:
-            self.load_default_namespaces()
-
-        self.separators = '/#:'
-        self.separator_re = re.compile(f'([{self.separators}])')
-
-        # overrides
-        self['https://global.dbpedia.org/id/'] = 'dbg'
-        self['http://www.wikidata.org/entity/'] = 'wde'
-
-        # reverse mapping
-        self.reverse_dict = {pf: ns for ns, pf in self.items()}
-
-    def qname(self, iri):
-        try:
-            namespace, local_name = self.split_iri(iri)
-        except ValueError:
-            return iri
-
-        if namespace in self:
-            return f'{self[namespace]}:{local_name}'
-        else:
-            return iri
-
-    def reverse(self, qname):
-        try:
-            prefix, local_name = qname.split(':', maxsplit=1)
-        except ValueError:
-            return qname
-
-        if prefix in self.reverse_dict:
-            return f'{self.reverse_dict[prefix]}{local_name}'
-        else:
-            return qname
-
-    def split_iri(self, iri):
-        iri_split = self.separator_re.split(iri)
-
-        local_parts = []
-        while iri_split:
-            *iri_split, local_part = iri_split
-            local_parts.append(local_part)
-            namespace = ''.join(iri_split)
-
-            if namespace in self:
-                local_name = ''.join(reversed(local_parts))
-                if local_name and local_name[0] in self.separators:
-                    local_name = local_name[1:]
-
-                return namespace, local_name
-
-        raise ValueError(f"Can't split '{iri}'")
-
-    def load_default_namespaces(self):
-        try:
-            ns_mapping = self.fetch_default_namespaces()
-        except (AttributeError, ConnectionError, RequestException):
-            print(f"Couldn't update namespaces from {self.default_namespaces_url}")
-            with open(self.default_namespaces_file) as ns_file:
-                ns_mapping = json.load(ns_file)
-
-        self.update(ns_mapping)
-
-    def fetch_default_namespaces(self):
-        print(f'Downloading namespaces from {self.default_namespaces_url} ...')
-        nsdecl_resp = requests.get(self.default_namespaces_url)
-        nsdecl_soup = BeautifulSoup(nsdecl_resp.text, 'lxml')
-        ns_table = nsdecl_soup.find('table', class_='tableresult')
-
-        ns_to_prefix = {}
-        for tr in ns_table.find_all('tr'):
-            prefix_td = tr.find('td')
-            namespace_a = tr.find('a')
-            if prefix_td and namespace_a:
-                ns_to_prefix[namespace_a['href']] = prefix_td.text
-
-        with open(self.default_namespaces_file, 'w') as ns_file:
-            json.dump(ns_to_prefix, ns_file, indent=4)
-
-        return ns_to_prefix
-
-
-@functools.lru_cache(maxsize=4096)
-def fetch_wikidata_uri(samething_service_url, resource_iri):
-    wikidata_base = f'http://www.wikidata.org/entity/'
-    canonical_iri = None
-    request_uri = f'{samething_service_url}lookup/?meta=off&uri={resource_iri}'
-    response = requests.get(request_uri)
-    if response.ok:
-        for iri in response.json()['locals']:
-            if iri.startswith(wikidata_base):
-                canonical_iri = iri
-                break
-
-    if not canonical_iri:
-        canonical_iri = resource_iri
-        print(f'same-thing: no Wikidata URI found by {request_uri}')
-
-    return canonical_iri
---
+++ dbpedia/uri_transformation.py
@@ -0,0 +1,123 @@
+import functools
+import json
+import re
+from collections import UserDict
+
+import requests
+from bs4 import BeautifulSoup
+from requests import RequestException
+
+from dbpedia.utils import base_path
+
+
+class NamespacePrefixer(UserDict):
+
+    def __init__(self, mapping=None, **kwargs):
+        super().__init__(mapping, **kwargs)
+        self.default_namespaces_url = 'http://dbpedia.org/sparql?nsdecl'
+        self.default_namespaces_file = base_path('default-namespaces.json')
+        if not self.data:
+            self.load_default_namespaces()
+
+        self.separators = '/#:'
+        self.separator_re = re.compile(f'([{self.separators}])')
+
+        # overrides
+        self['https://global.dbpedia.org/id/'] = 'dbg'
+        self['http://www.wikidata.org/entity/'] = 'wde'
+
+        # reverse mapping
+        self.reverse_dict = {pf: ns for ns, pf in self.items()}
+
+    def qname(self, iri):
+        try:
+            namespace, local_name = self.split_iri(iri)
+        except ValueError:
+            return iri
+
+        if namespace in self:
+            return f'{self[namespace]}:{local_name}'
+        else:
+            return iri
+
+    def reverse(self, qname):
+        try:
+            prefix, local_name = qname.split(':', maxsplit=1)
+        except ValueError:
+            return qname
+
+        if prefix in self.reverse_dict:
+            return f'{self.reverse_dict[prefix]}{local_name}'
+        else:
+            return qname
+
+    def split_iri(self, iri):
+        iri_split = self.separator_re.split(iri)
+
+        local_parts = []
+        while iri_split:
+            *iri_split, local_part = iri_split
+            local_parts.append(local_part)
+            namespace = ''.join(iri_split)
+
+            if namespace in self:
+                local_name = ''.join(reversed(local_parts))
+                if local_name and local_name[0] in self.separators:
+                    local_name = local_name[1:]
+
+                return namespace, local_name
+
+        raise ValueError(f"Can't split '{iri}'")
+
+    def load_default_namespaces(self):
+        try:
+            ns_mapping = self.fetch_default_namespaces()
+        except (AttributeError, ConnectionError, RequestException):
+            print(f"Couldn't update namespaces from {self.default_namespaces_url}")
+            with open(self.default_namespaces_file) as ns_file:
+                ns_mapping = json.load(ns_file)
+
+        self.update(ns_mapping)
+
+    def fetch_default_namespaces(self):
+        print(f'Downloading namespaces from {self.default_namespaces_url} ...')
+        nsdecl_resp = requests.get(self.default_namespaces_url)
+        nsdecl_soup = BeautifulSoup(nsdecl_resp.text, 'lxml')
+        ns_table = nsdecl_soup.find('table', class_='tableresult')
+
+        ns_to_prefix = {}
+        for tr in ns_table.find_all('tr'):
+            prefix_td = tr.find('td')
+            namespace_a = tr.find('a')
+            if prefix_td and namespace_a:
+                ns_to_prefix[namespace_a['href']] = prefix_td.text
+
+        with open(self.default_namespaces_file, 'w') as ns_file:
+            json.dump(ns_to_prefix, ns_file, indent=4)
+
+        return ns_to_prefix
+
+
+class SameThingClient:
+    wikidata_base = f'http://www.wikidata.org/entity/'
+
+    def __init__(self, samething_service_url):
+        self.samething_service_url = samething_service_url
+        self.session = requests.Session()
+
+    @functools.lru_cache(maxsize=4096)
+    def fetch_wikidata_uri(self, resource_iri):
+        canonical_iri = None
+        request_uri = f'{self.samething_service_url}lookup/?meta=off&uri={resource_iri}'
+        response = self.session.get(request_uri)
+        if response.ok:
+            for iri in response.json()['locals']:
+                if iri.startswith(self.wikidata_base):
+                    canonical_iri = iri
+                    break
+
+        if not canonical_iri:
+            canonical_iri = resource_iri
+            print(f'same-thing: no Wikidata URI found by {request_uri}')
+
+        return canonical_iri
